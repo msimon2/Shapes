3ca0b2f82b08d648924bd62f1b2efc19d403977d
msimon2 <msimon2016@gmail.com>
2015-10-23 07:08:58 -0600

    not done

--- Shapes/src/Equilateral.java
+++ Shapes/src/Equilateral.java
@@ -1,5 +1,10 @@
 
 public class Equilateral
 	{
-
+	protected double side;
+	
+	public double findPerimeter()
+		{
+		return side*3;
+		}
 	}
--- Shapes/src/Rectangle.java
+++ Shapes/src/Rectangle.java
@@ -1,5 +1,15 @@
 
-public class Rectangle
+public class Rectangle extends Quadrilateral
 	{
-
+		protected double length;
+		protected double width;
+	public Rectangle(double l, double w)
+		{
+		length = l;
+		width = w;
+		}
+	public double findArea()
+		{
+		return length*width;	
+		}
 	}
